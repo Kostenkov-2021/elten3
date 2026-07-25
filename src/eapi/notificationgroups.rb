d71d9d5aa7ed91e4553a8dbce499f6a115715839
Dawid Pieper <dawidpieper@o2.pl>
2026-07-25 20:39:22 +0200

    Return to main window from blog notifications

--- src/eapi/notificationgroups.rb
+++ src/eapi/notificationgroups.rb
@@ -479,7 +479,7 @@ module NotificationGroups
     post.name = payload["title"].to_s
     post.author = payload["author"].to_s
     post.mention = blog_mention_from_payload(payload) if cat.to_s == "blogmention" && payload["mentionid"].to_i > 0
-    insert_scene(Scene_Blog_Read.new(post, -1, 0, 0), true)
+    insert_scene(Scene_Blog_Read.new(post, -1, 0, 0, Scene_Main.new), true, return_to_main: true)
   end
 
   def blog_mention_from_payload(payload)
