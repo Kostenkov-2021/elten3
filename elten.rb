8a80a0b3b554bbb05c6ad538210c13b2c6a1c2d1
Dawid Pieper <dawidpieper@o2.pl>
2026-08-20 17:58:32 +0200

    Bump version to ELTEN 3.0 RC 3

--- elten.rb
+++ elten.rb
@@ -3,7 +3,7 @@ Dir.chdir(root)
 $LOAD_PATH.unshift(File.join(root, "src")) unless defined?(::EltenEmbedded)
 
 module Elten
-  VERSION_STRING = "ELTEN 3.0 RC 2"
+  VERSION_STRING = "ELTEN 3.0 RC 3"
   BRANCH = "rc"
 
   class << self
